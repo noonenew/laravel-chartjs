b280ca61158d7845219bf391f388afe6ed4df596
Noonenew <30725615+noonenew@users.noreply.github.com>
2019-07-18 07:51:53 +0300

    Update ChartjsServiceProvider.php

--- src/Providers/ChartjsServiceProvider.php
+++ src/Providers/ChartjsServiceProvider.php
@@ -1,10 +1,10 @@
-<?php namespace Fx3costa\LaravelChartJs\Providers;
+<?php namespace Noonenew\LaravelChartJs\Providers;
 
-use Fx3costa\LaravelChartJs\Builder;
-use Fx3costa\LaravelChartJs\ChartBar;
-use Fx3costa\LaravelChartJs\ChartLine;
-use Fx3costa\LaravelChartJs\ChartPieAndDoughnut;
-use Fx3costa\LaravelChartJs\ChartRadar;
+use Noonenew\LaravelChartJs\Builder;
+use Noonenew\LaravelChartJs\ChartBar;
+use Noonenew\LaravelChartJs\ChartLine;
+use Noonenew\LaravelChartJs\ChartPieAndDoughnut;
+use Noonenew\LaravelChartJs\ChartRadar;
 use Illuminate\Support\ServiceProvider;
 
 class ChartjsServiceProvider extends ServiceProvider
